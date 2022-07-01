691710486b7375e84928b198dcc3d9afade410cd
DEMETRIUS CARNEIRO MONTEIRO FERREIRA <68928535+demithehomie@users.noreply.github.com>
2022-07-01 11:47:43 -0300

    First Commit

---
+++ APRENDENDO/1x.py
@@ -0,0 +1,18 @@
+import pyautogui
+import time
+pyautogui.KEYBOARD_KEYS
+pyautogui.PAUSE = 0.5
+
+#abrir o google drive 
+pyautogui.press('win')
+#entrar na area de trabalho
+pyautogui.write('chrome')
+pyautogui.press('enter')
+time.sleep(5)
+pyautogui.write('youtube.com')
+pyautogui.press('enter')
+#clicar e arrastar o arquivo
+pyautogui.mouseDown
+#arrastando eu mermo já entrei no google drive
+#soltei o arquivo lá
+#esperar alguns segundos
---
+++ APRENDENDO/2x.py
@@ -0,0 +1,25 @@
+import pyautogui
+import time
+pyautogui.KEYBOARD_KEYS
+pyautogui.PAUSE = 0.5
+
+ 
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+time.sleep(5)
+pyautogui.write('youtube.com')
+pyautogui.press('enter')
+time.sleep(5)
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('tab')
+
+pyautogui.write('CS50')
+pyautogui.press('enter')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('enter')
